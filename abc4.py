7763217d9b75b0a47ad830614999fc6da8d87c46
shahjiiya <132895429+shahjiiya@users.noreply.github.com>
2023-05-28 20:51:35 +0530

    Create abc4.py

---
+++ abc4.py
@@ -0,0 +1,25 @@
+#1
+#string_Mississippi
+
+def my_function():
+    Mississippi="i =04 s =04 p =02 m =01"
+    myDict={'i=':'04',
+        's=':'04',
+        'p=':'02',
+        'm=':'01'}
+    print(Mississippi)
+    
+my_function()
+
+print("or")
+
+#2
+def my_function():
+    Mississippi="i =04 s =04 p =02 m =01"
+    myDict={'i=':'04',
+        's=':'04',
+        'p=':'02',
+        'm=':'01'}
+    print(myDict)
+    
+my_function()
